8ab71aac88efc0e5c0ab95c5c8d886e3f5b5ef22
David Grammatico <david.lugdunum@gmail.com>
2023-09-07 17:05:27 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center w-screen h-screen">
+      <h1 className="text-4xl font-bold">
+        Let&apos;s go to create a beautiful app with next.js !
+      </h1>
+    </main>
+  )
+}
