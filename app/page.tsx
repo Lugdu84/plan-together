6eea49f86186b56dc0facf7c70a8da7b9694a7e7
melcd238 <melcd238@gmail.com>
2023-09-13 12:55:57 +0200

    feature-#24-Basic HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,24 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser } from '@fortawesome/free-solid-svg-icons'
-
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center w-screen h-screen">
-      <h1 className="text-4xl font-bold">
-        Let&apos;s go to create a beautiful app with next.js !
+    <main className="flex flex-col justify-center w-screen h-screen ml-24">
+      <h1 className="text-4xl font-bold pb-10 tracking-wide">
+        Planifiez facilement vos activités <br /> avec vos ami.e.s
       </h1>
-      <div>
-        <FontAwesomeIcon icon={faUser} />
+      <p className="text-gray-500">
+        Finies les difficultés pour programmer vos sorties ou activités en
+        groupe.
+      </p>
+      <p className="text-gray-500">PlanTogether s&apos;occupe de tout.</p>
+      <div className="flex space-x-4 mt-8">
+        <button type="button" className="bg-black text-white px-4 py-2 rounded">
+          Commencer
+        </button>
+        <button
+          type="button"
+          className="bg-white text-black border border-black px-4 py-2 rounded"
+        >
+          En savoir plus
+        </button>
       </div>
     </main>
   )
