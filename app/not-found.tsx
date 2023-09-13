4e03658d17a854689fcf424c5042517adc3830b5
melcd238 <melcd238@gmail.com>
2023-09-13 11:22:12 +0200

    feature-#23 Set up architecture

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div>
+      <h1>404</h1>
+      <p> Oups, la page que vous demandez n&apos;existe pas </p>
+    </div>
+  )
+}
+
+export default NotFound
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center w-screen h-screen">
       <h1 className="text-4xl font-bold">
         Let&apos;s go to create a beautiful app with next.js !
       </h1>
+      <div>
+        <FontAwesomeIcon icon={faUser} />
+      </div>
     </main>
   )
 }
