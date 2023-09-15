8a288c61f4ba5b7609f289553bb10696dcf74480
melcd238 <melcd238@gmail.com>
2023-09-15 19:14:17 +0200

    registerForm and route

---
+++ app/api/register/route.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from '@prisma/client';
+import { hash } from 'bcryptjs';
+import { NextResponse } from 'next/server';
+
+const prisma = new PrismaClient();
+
+// eslint-disable-next-line import/prefer-default-export
+export async function POST(req: Request) {
+  try {
+    const { firstname, lastname, email, password } = (await req.json()) as {
+      firstname: string;
+      lastname: string;
+      email: string;
+      password: string;
+    };
+    const hashpw = await hash(password, 10);
+    const user = await prisma.user.create({
+      data: {
+        firstname,
+        lastname,
+        email,
+        password: hashpw,
+      },
+    });
+    return NextResponse.json({
+      user: {
+        firstname: user.firstname,
+        lastname: user.lastname,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    // TODO: implement toast
+    console.error(error);
+    // return new NextResponse();
+  }
+}
---
+++ app/register/page.tsx
@@ -0,0 +1,10 @@
+import RegisterForm from './form';
+
+export default function RegisterPage() {
+  return (
+    <div>
+      <h1>Register</h1>
+      <RegisterForm />
+    </div>
+  );
+}
