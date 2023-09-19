eb382ae6c2c676b32a746e6b483038892fdcfc13
Olivia D. S <hello@palingenae.be>
2023-09-19 11:13:37 +0200

    update: login form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,9 +2,13 @@ import LoginForm from './form';
 
 export default function LoginPage() {
   return (
-    <div>
-      <h1>Login</h1>
-      <LoginForm />
-    </div>
+    <main className="flex flex-col justify-center items-center align-middle w-screen h-screen lg:px-16 xl:px-32">
+      <section className="flex flex-col justify-center items-center gap-1 w-2/6">
+        <h1 className="font-serif text-xl md:text-5xl lg:text-6xl xl:text-7xl leading-tight pb-10">
+          Login
+        </h1>
+        <LoginForm />
+      </section>
+    </main>
   );
 }
