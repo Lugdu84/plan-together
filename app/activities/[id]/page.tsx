120a495bb47fd5dbaff7f1c63b49fea59f4f250a
Olivia D. S <hello@palingenae.be>
2023-09-20 16:42:25 +0200

    Update: display invitations

--- app/activities/[id]/page.tsx
+++ app/activities/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { InvitationStatus } from '@prisma/client';
 import prisma from '@/app/utilities/prismadb';
 
 const metadata: {
@@ -12,17 +13,21 @@ async function Activity({ params }: { params: { id: string } }) {
       id: +params.id,
     },
     include: {
+      Invitation: {
+        include: {
+          User: true,
+        },
+      },
       User: true,
-      Invitation: true,
     },
   });
 
   return (
-    <main className="flex flex-col items-center w-screen h-screen pt-16 lg:px-16 xl:px-32">
-      <div className="flex flex-col w-3/5 gap-16">
+    <main className="flex flex-col items-center w-screen py-32 lg:px-16 xl:px-32">
+      <div className="flex flex-col w-3/5 gap-16 max-w-3xl">
         <header className="flex flex-col gap-16">
           <div className="flex flex-col gap-4">
-            <h1 className="text-2xl font-serif italic md:text-5xl mb-8">
+            <h1 className="text-2xl font-serif italic md:text-5xl mb-4">
               {activity.title}
             </h1>
             <p className="text-gray-500">{activity.type}</p>
@@ -41,8 +46,49 @@ async function Activity({ params }: { params: { id: string } }) {
             <p className="text-xl prose max-w-prose">{activity.description}</p>
           </article>
         </section>
-        <section>
+        <section className="flex flex-col gap-4">
           <h2 className="text-4xl font-serif mb-8">Personnes invitées</h2>
+          {activity.Invitation.map((invitation) => (
+            <div className="flex justify-between w-1/2">
+              <span
+                className={
+                  invitation.status === InvitationStatus.EXPIRED
+                    ? 'inline text-lg font-medium text-gray-400'
+                    : 'inline text-lg font-medium'
+                }
+              >
+                {invitation.User.firstname} {invitation.User.lastname}
+              </span>
+              {invitation.status === InvitationStatus.VALIDATED ? (
+                <span className="inline font-bold py-0.5 px-2 rounded-md text-emerald-800 bg-emerald-100">
+                  Présent-e
+                </span>
+              ) : (
+                ''
+              )}
+              {invitation.status === InvitationStatus.PENDING ? (
+                <span className="inline py-0.5 px-2 rounded-md text-amber-700 bg-amber-100">
+                  En attente
+                </span>
+              ) : (
+                ''
+              )}
+              {invitation.status === InvitationStatus.REFUSED ? (
+                <span className="inline py-0.5 px-2 rounded-md text-red-700 bg-red-100">
+                  Absent-e
+                </span>
+              ) : (
+                ''
+              )}
+              {invitation.status === InvitationStatus.EXPIRED ? (
+                <span className="inline py-0.5 px-2 rounded-md text-gray-400 bg-gray-200">
+                  Expiré
+                </span>
+              ) : (
+                ''
+              )}
+            </div>
+          )).sort()}
         </section>
       </div>
     </main>
